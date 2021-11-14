07571db1c26e1743949a8727e2c74d70bef6ea86
binoy638 <binoy638@gmail.com>
2021-11-14 19:12:09 +0530

    test

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 
 @app.route('/abc',)
-# @app.route('/<path:path>')
+@app.route('/<path:path>')
 def index(path):
     return Response("<h1>Flask</h1><p>You visited:</p>", mimetype="text/html")
 
