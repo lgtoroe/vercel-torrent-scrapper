3d2003d3f5080eb6cf1221ccc6a60bedcd4d87ac
binoy638 <binoy638@gmail.com>
2021-11-14 19:25:10 +0530

    test

--- api/index.py
+++ api/index.py
@@ -3,16 +3,16 @@ from sanic.response import json
 app = Sanic()
 
 
-@app.route('/')
-@app.route('/<path:path>')
+@app.route('/abc')
+@app.route('/abc/<path:path>')
 async def index(request, path=""):
     return json({'hello': path})
 
 
-@app.route('/other_route')
-@app.route('/<path:path>')
-async def other_route(request, path=""):
-    return json({'whatever': path})
+# @app.route('/other_route')
+# @app.route('/<path:path>')
+# async def other_route(request, path=""):
+#     return json({'whatever': path})
 
 # @app.route("/gg")
 # def other():
