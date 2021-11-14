88a97836f62d1a2e94a7e6b11d93178bae3467b8
binoy638 <binoy638@gmail.com>
2021-11-14 19:52:45 +0530

    scrapper fn

--- api/index.py
+++ api/index.py
@@ -1,13 +1,10 @@
-from flask import Flask, Response
-app = Flask(__name__)
+from sanic import Sanic
+from sanic.response import json
+from utils.scrapper import searchRarbg
+app = Sanic()
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return {'hasdsad': path}
-
-
-# @app.route('/abc/<path:path>')
-# async def index(request, path=""):
-#     return json({'hasdsad': path})
+@app.route('/abc/<path:path>')
+async def index(request, path=""):
+    result = searchRarbg("avengers")
+    return json({'hasdsad': result})
