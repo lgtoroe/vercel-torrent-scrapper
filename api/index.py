ccd36d06778b578821e5f1dbae40879540219297
binoy638 <binoy638@gmail.com>
2021-11-14 20:40:05 +0530

    test

--- api/index.py
+++ api/index.py
@@ -1,17 +1,30 @@
 from sanic import Sanic
 from sanic.response import json
 from utils.scrapper import searchRarbg
+# app = Sanic()
+
+
 app = Sanic()
 
 
 @app.route('/')
 @app.route('/<path:path>')
-async def search(request, path=""):
-    result = searchRarbg("avengers")
-    return json({'results': result})
+async def index(request, path=""):
+    return json({'hello': path})
 
 
-@app.route('/test')
-async def search2(request, path="test"):
-    result = searchRarbg("game of thrones")
-    return json({'results': result})
+@app.route('/other_route')
+async def other_route(request, path="other_route"):
+    return json({'whatever': path})
+
+# @app.route('/')
+# @app.route('/<path:path>')
+# async def search(request, path=""):
+#     result = searchRarbg("avengers")
+#     return json({'results': result})
+
+
+# @app.route('/test')
+# async def search2(request, path="test"):
+#     result = searchRarbg("game of thrones")
+#     return json({'results': result})
