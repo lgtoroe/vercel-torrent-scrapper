b01c9db15829152d406da32abb4af02cbd95d4e6
binoy638 <binoy638@gmail.com>
2021-11-14 18:51:25 +0530

    test

--- api/test.py
+++ api/test.py
@@ -1,11 +1,8 @@
 from flask import Flask, Response
-from utils.scrapper import searchRarbg
-
-
 app = Flask(__name__)
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return {"message": "hello world"}
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
