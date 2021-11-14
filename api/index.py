5f75a8a96091f774128b833cd82ea76a1040a7a8
binoy638 <binoy638@gmail.com>
2021-11-14 22:43:01 +0530

    bug fix

--- api/index.py
+++ api/index.py
@@ -8,11 +8,59 @@ app = Sanic()
 @app.route('/<path:path>')
 async def index(request, path=""):
     if path.startswith("api/rarbg"):
-        handle_rarbg_route(path, request)
+        # handle_rarbg_route(path, request)
+        if path.startswith("api/rarbg/search"):
+            query = request.args.get("q")
+            if not query:
+                return json({'message': "empty query"}, status=404)
+            result = searchRarbg(query)
+            if not result:
+                return json({'message': "no results found"}, status=404)
+            return json({"result": result})
+        if path.startswith("api/rarbg/get"):
+            link = request.args.get("link")
+            if not link:
+                return json({'message': "no link provided"}, status=404)
+            result = getRarbgTorrentData(link)
+            if not result:
+                return json({'message': "no results found"}, status=404)
+            return json({"result": result})
     elif path.startswith("api/piratebay"):
-        handle_piratebay_route(path, request)
+        # handle_piratebay_route(path, request)
+        if path.startswith("api/piratebay/search"):
+            query = request.args.get("q")
+            if not query:
+                return json({'message': "empty query"}, status=404)
+            result = searchTPB(query)
+            if not result:
+                return json({'message': "no results found"}, status=404)
+            return json({"result": result})
+        if path.startswith("api/piratebay/get"):
+            link = request.args.get("link")
+            if not link:
+                return json({'message': "no link provided"}, status=404)
+            result = getTPBTorrentData(link)
+            if not result:
+                return json({'message': "no results found"}, status=404)
+            return json({"result": result})
     elif path.startswith("api/threesven"):
-        handle_threesven_route(path, request)
+        # handle_threesven_route(path, request)
+        if path.startswith("api/threesven/search"):
+            query = request.args.get("q")
+            if not query:
+                return json({'message': "empty query"}, status=404)
+            result = search1337x(query)
+            if not result:
+                return json({'message': "no results found"}, status=404)
+            return json({"result": result})
+        if path.startswith("api/threesven/get"):
+            link = request.args.get("link")
+            if not link:
+                return json({'message': "no link provided"}, status=404)
+            result = get1337xTorrentData(link)
+            if not result:
+                return json({'message': "no results found"}, status=404)
+            return json({"result": result})
     else:
         return json({'message': "invalid path"}, status=404)
 
