b3ae60c92e55c99c3f41a3d205dbd9d59e3af39b
binoy638 <binoy638@gmail.com>
2021-11-14 21:29:21 +0530

    test

--- api/index.py
+++ api/index.py
@@ -13,8 +13,8 @@ def catch_all(path):
     #     return jsonify({'results': result})
 
 
-@app.route('/gg', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/gg')
+@app.route('/gg/<path:path>')
 def catch_all2(path):
     result = searchRarbg("game of thrones")
     return jsonify({'results': result})
