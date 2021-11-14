ff1d623080583870ce9b8a14af86d91d063272d8
binoy638 <binoy638@gmail.com>
2021-11-14 19:56:34 +0530

    test

--- api/index.py
+++ api/index.py
@@ -4,8 +4,8 @@ from utils.scrapper import searchRarbg
 app = Sanic()
 
 
-@app.route('/abc')
-@app.route('/abc/<path:path>')
+@app.route('/')
+@app.route('/<path:path>')
 async def index(request, path=""):
     result = searchRarbg("avengers")
     return json({'hasdsad': result})
