32da348565b43f87063115cd7b41cf771d38c00b
binoy638 <binoy638@gmail.com>
2021-11-14 20:50:51 +0530

    test

--- api/index.py
+++ api/index.py
@@ -1,21 +1,52 @@
-from sanic import Sanic
-from sanic.response import json
-from utils.scrapper import searchRarbg
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Home Page Route - nice work Andrew!!!'
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
+
+
+# from sanic import Sanic
+# from sanic.response import json
+# from utils.scrapper import searchRarbg
 # app = Sanic()
 
 
-app = Sanic()
-
-
 # @app.route('/<path:path>')
-@app.route('/main')
-async def index(request, path=""):
-    return json({'hello': path})
+# # @app.route('/main')
+# async def index(request, path=""):
+#     return json({'hello': path})
 
 
-@app.route('/other_route')
-async def other_route(request, path="other_route"):
-    return json({'whatever': path})
+# @app.route('/other_route')
+# async def other_route(request, path="other_route"):
+#     return json({'whatever': path})
 
 # @app.route('/')
 # @app.route('/<path:path>')
