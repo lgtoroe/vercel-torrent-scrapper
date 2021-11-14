e266d354bce3dac9bf5b00053dd3a4b1bbdb70f3
binoy638 <binoy638@gmail.com>
2021-11-14 22:16:47 +0530

    rarbg get torrent from link

--- api/index.py
+++ api/index.py
@@ -1,16 +1,26 @@
 from sanic import Sanic
 from sanic.response import json
-from utils.scrapper import searchRarbg
+from utils.scrapper import searchRarbg, getRarbgTorrentData
 app = Sanic()
 
 
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
-    if path == "api/search":
+    if path.startswith("api/search"):
         query = request.args.get("q")
         if not query:
             return json({'message': "empty query"}, status=404)
         result = searchRarbg(query)
+        if not result:
+            return json({'message': "no results found"}, status=404)
+        return json({"result": result})
+    if path.startswith("api/get"):
+        link = request.args.get("link")
+        if not link:
+            return json({'message': "no link provided"}, status=404)
+        result = getRarbgTorrentData(link)
+        if not result:
+            return json({'message': "no results found"}, status=404)
         return json({"result": result})
     return json({'message': "invalid path"}, status=404)
