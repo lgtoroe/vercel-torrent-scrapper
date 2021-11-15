2edf447e9662fba9b076fe9bf7f448ca61319a69
binoy638 <binoy638@gmail.com>
2021-11-15 10:32:36 +0530

    added cors

--- api/index.py
+++ api/index.py
@@ -132,4 +132,6 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
+    if path == "api/search":
+        return {'message': "asdasd"}
     return {'message': "empty query"}
