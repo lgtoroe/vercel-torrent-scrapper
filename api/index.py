04f9c7aef33c33aa1a5338432334dcc4213d9a4a
binoy638 <binoy638@gmail.com>
2021-11-16 21:45:35 +0530

    added nyaa

--- api/index.py
+++ api/index.py
@@ -1,133 +1,6 @@
-# from sanic import Sanic
-# from sanic.response import json
-# from sanic_cors import CORS, cross_origin
-
-# from utils.scrapper import searchRarbg, getRarbgTorrentData, searchTPB, getTPBTorrentData, search1337x, get1337xTorrentData
-# app = Sanic(__name__)
-# CORS(app, resources={r"/api/*": {"origins": "*"}})
-
-
-# @app.route('/')
-# @app.route('/<path:path>')
-# @cross_origin(app)
-# async def index(request, path=""):
-#     if path.startswith("api/rarbg"):
-#         # handle_rarbg_route(path, request)
-#         if path.startswith("api/rarbg/search"):
-#             query = request.args.get("q")
-#             if not query:
-#                 return json({'message': "empty query"}, status=404)
-#             result = searchRarbg(query)
-#             if not result:
-#                 return json({'message': "no results found"}, status=404)
-#             return json({"result": result})
-#         if path.startswith("api/rarbg/get"):
-#             link = request.args.get("link")
-#             if not link:
-#                 return json({'message': "no link provided"}, status=404)
-#             result = getRarbgTorrentData(link)
-#             if not result:
-#                 return json({'message': "no results found"}, status=404)
-#             return json({"result": result})
-#     elif path.startswith("api/piratebay"):
-#         # handle_piratebay_route(path, request)
-#         if path.startswith("api/piratebay/search"):
-#             query = request.args.get("q")
-#             if not query:
-#                 return json({'message': "empty query"}, status=404)
-#             result = searchTPB(query)
-#             if not result:
-#                 return json({'message': "no results found"}, status=404)
-#             return json({"result": result})
-#         if path.startswith("api/piratebay/get"):
-#             link = request.args.get("link")
-#             if not link:
-#                 return json({'message': "no link provided"}, status=404)
-#             result = getTPBTorrentData(link)
-#             if not result:
-#                 return json({'message': "no results found"}, status=404)
-#             return json({"result": result})
-#     elif path.startswith("api/threesven"):
-#         # handle_threesven_route(path, request)
-#         if path.startswith("api/threesven/search"):
-#             query = request.args.get("q")
-#             if not query:
-#                 return json({'message': "empty query"}, status=404)
-#             result = search1337x(query)
-#             if not result:
-#                 return json({'message': "no results found"}, status=404)
-#             return json({"result": result})
-#         if path.startswith("api/threesven/get"):
-#             link = request.args.get("link")
-#             if not link:
-#                 return json({'message': "no link provided"}, status=404)
-#             result = get1337xTorrentData(link)
-#             if not result:
-#                 return json({'message': "no results found"}, status=404)
-#             return json({"result": result})
-#     else:
-#         return json({'message': "invalid path"}, status=404)
-
-
-# def handle_rarbg_route(path, request):
-#     if path.startswith("api/rarbg/search"):
-#         query = request.args.get("q")
-#         if not query:
-#             return json({'message': "empty query"}, status=404)
-#         result = searchRarbg(query)
-#         if not result:
-#             return json({'message': "no results found"}, status=404)
-#         return json({"result": result})
-#     if path.startswith("api/rarbg/get"):
-#         link = request.args.get("link")
-#         if not link:
-#             return json({'message': "no link provided"}, status=404)
-#         result = getRarbgTorrentData(link)
-#         if not result:
-#             return json({'message': "no results found"}, status=404)
-#         return json({"result": result})
-
-
-# def handle_piratebay_route(path, request):
-#     if path.startswith("api/piratebay/search"):
-#         query = request.args.get("q")
-#         if not query:
-#             return json({'message': "empty query"}, status=404)
-#         result = searchTPB(query)
-#         if not result:
-#             return json({'message': "no results found"}, status=404)
-#         return json({"result": result})
-#     if path.startswith("api/piratebay/get"):
-#         link = request.args.get("link")
-#         if not link:
-#             return json({'message': "no link provided"}, status=404)
-#         result = getTPBTorrentData(link)
-#         if not result:
-#             return json({'message': "no results found"}, status=404)
-#         return json({"result": result})
-
-
-# def handle_threesven_route(path, request):
-#     if path.startswith("api/threesven/search"):
-#         query = request.args.get("q")
-#         if not query:
-#             return json({'message': "empty query"}, status=404)
-#         result = search1337x(query)
-#         if not result:
-#             return json({'message': "no results found"}, status=404)
-#         return json({"result": result})
-#     if path.startswith("api/threesven/get"):
-#         link = request.args.get("link")
-#         if not link:
-#             return json({'message': "no link provided"}, status=404)
-#         result = get1337xTorrentData(link)
-#         if not result:
-#             return json({'message': "no results found"}, status=404)
-#         return json({"result": result})
-
 from flask import Flask, request
 from flask_cors import CORS
-from utils.scrapper import searchRarbg, getRarbgTorrentData, searchTPB, getTPBTorrentData, search1337x, get1337xTorrentData
+from utils.scrapper import searchRarbg, getRarbgTorrentData, searchTPB, getTPBTorrentData, search1337x, get1337xTorrentData, searchNyaa
 app = Flask(__name__)
 CORS(app)
 
@@ -190,62 +63,15 @@ def catch_all(path):
             if not result:
                 return {'message': "no results found"}, 404
             return {"result": result}
+    elif path.startswith("api/nyaa"):
+        # handle_threesven_route(path, request)
+        if path.startswith("api/nyaa/search"):
+            query = request.args.get("q")
+            if not query:
+                return {'message': "empty query"}, 404
+            result = searchNyaa(query)
+            if not result:
+                return {'message': "no results found"}, 404
+            return {"result": result}
     else:
         return {'message': "invalid path"}
-
-
-# def handle_rarbg_route(path, request):
-#     if path.startswith("api/rarbg/search"):
-#         query=request.args.get("q")
-#         if not query:
-#             return {'message': "empty query"}, 404
-#         result=searchRarbg(query)
-#         if not result:
-#             return {'message': "no results found"}, 404
-#         return {"result": result}
-#     if path.startswith("api/rarbg/get"):
-#         link=request.args.get("link")
-#         if not link:
-#             return {'message': "no link provided"}, 404
-#         result=getRarbgTorrentData(link)
-#         if not result:
-#             return {'message': "no results found"}, 404
-#         return {"result": result}
-
-
-# def handle_piratebay_route(path, request):
-#     if path.startswith("api/piratebay/search"):
-#         query=request.args.get("q")
-#         if not query:
-#             return {'message': "empty query"}, 404
-#         result=searchTPB(query)
-#         if not result:
-#             return {'message': "no results found"}, 404
-#         return {"result": result}
-#     if path.startswith("api/piratebay/get"):
-#         link=request.args.get("link")
-#         if not link:
-#             return {'message': "no link provided"}, 404
-#         result=getTPBTorrentData(link)
-#         if not result:
-#             return {'message': "no results found"}, 404
-#         return {"result": result}
-
-
-# def handle_threesven_route(path, request):
-#     if path.startswith("api/threesven/search"):
-#         query=request.args.get("q")
-#         if not query:
-#             return {'message': "empty query"}, 404
-#         result=search1337x(query)
-#         if not result:
-#             return {'message': "no results found"}, 404
-#         return {"result": result}
-#     if path.startswith("api/threesven/get"):
-#         link=request.args.get("link")
-#         if not link:
-#             return {'message': "no link provided"}, 404
-#         result=get1337xTorrentData(link)
-#         if not result:
-#             return {'message': "no results found"}, 404
-#         return {"result": result}
--- utils/scrapper.py
+++ utils/scrapper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.exceptions import Timeout
 
 
 def toInt(value):
@@ -147,3 +148,46 @@ def getRarbgTorrentData(link):
         files.append(li.text.strip())
     data["files"] = files
     return data
+
+
+def searchNyaa(query):
+    url = f"https://nyaa.si/?f=0&c=0_0&q={query}&s=seeders&o=desc"
+    try:
+        results = requests.get(url, timeout=3)
+    except Timeout as excep:
+        print("Could not connect to the site...", excep)
+        return None
+
+    soup = BeautifulSoup(results.text, "html.parser")
+
+    try:
+        table = soup.find("table", class_="table").find("tbody")
+    except AttributeError:
+        return []
+
+    animes = table.find_all("tr")
+
+    if not animes:
+        return []
+
+    anime_list = []
+
+    for anime in animes:
+
+        data = anime.find_all("td")
+        try:
+            comment = data[1].find("a", class_="comments")
+            comment.decompose()
+        except Exception as excep:
+            title = data[1].text.strip()
+        finally:
+            title = data[1].text.strip()
+            magnet = data[2].find("a").findNext("a")["href"]
+            size = data[3].text.strip()
+            seed = data[5].text.strip()
+            leech = data[6].text.strip()
+            file = "https://nyaa.si" + data[2].find("a")["href"]
+            uploader = "unknown"
+            anime_list.append({"name": title, "link": magnet, "size": size,
+                               "seeds": seed, "leeches": leech, "uploader": uploader, "file": file})
+    return anime_list
